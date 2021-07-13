aad30131530fdbeea7745bc8dec15fc5386ee046
BonnyWang <bonny.y.gardy@gmail.com>
2021-07-13 03:09:55 -0400

    Gravitational added

--- Assets/Script/gravitationalF.cs
+++ Assets/Script/gravitationalF.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class gravitationalF : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // temporaral to test two object system
+    public GameObject otherObj;
+    const float gConst = 600;
+    public static List<gravitationalF> Bodies;
+    public Rigidbody rb { get; protected set; }
+
+
+    double gForce;
+
     void Start()
     {
-        
+
+        rb = GetComponent<Rigidbody>();
+
+        if (Bodies == null){
+			Bodies = new List<gravitationalF>();
+        }
+
+        Bodies.Add(this);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    private void FixedUpdate() {
         
+        foreach( gravitationalF body in Bodies){
+            if(body == this){
+                continue;
+            }
+
+
+            // Calculate the distance between the two body
+            float r = Vector3.Distance (transform.position, body.transform.position);
+
+            float gForceMag = gConst*( rb.mass* body.rb.mass) / Mathf.Pow (r, 2);
+
+            Vector3 forceDirection = Vector3.Normalize (body.transform.position - transform.position);
+
+            Vector3 gForce = (forceDirection*gForceMag)* Time.fixedDeltaTime;
+
+            rb.AddForce(gForce);
+        }
     }
 }
