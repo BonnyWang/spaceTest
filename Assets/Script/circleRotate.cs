790edb842bafbfe07ea1394b45992e66e00aea9d
BonnyWang <bonny.y.gardy@gmail.com>
2021-07-13 04:40:58 -0400

    psudo center

--- Assets/Script/circleRotate.cs
+++ Assets/Script/circleRotate.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class circleRotate : MonoBehaviour
 {
-    public GameObject target;
-    [SerializeField] private int rotateSpeed;
+    public float initialVelocity;
     float scaleSize;
+    [SerializeField] private int rotateSpeed;
+
+    private GameObject target;
     private void Start() {
         target = GameObject.Find("Center");
+        GetComponent<Rigidbody>().velocity = new Vector3(initialVelocity,initialVelocity,initialVelocity);
 
         scaleSize = Random.Range(0.2f,1);
         rotateSpeed = Random.Range(10,200);
@@ -18,7 +21,6 @@ public class circleRotate : MonoBehaviour
         // Update is called once per frame
     void Update()
     {
-
         transform.RotateAround(target.transform.position, Vector3.forward,  rotateSpeed* Time.deltaTime);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # spaceTest-Unity
  spaceGame Testing with Unity
+
+ ## Todo
+ - Change the motion from position to velocity
+ - add an interglaxy planet
+ - Downlaod Skybox and planet material
