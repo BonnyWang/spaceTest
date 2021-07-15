cbd54cafd28aa3446d6696f503485be651516ec4
BonnyWang <bonny.y.gardy@gmail.com>
2021-07-15 02:40:19 -0400

    Try shader

--- Assets/Script/circleRotate.cs
+++ Assets/Script/circleRotate.cs
@@ -16,11 +16,14 @@ public class circleRotate : MonoBehaviour
         scaleSize = Random.Range(0.2f,1);
         rotateSpeed = Random.Range(10,200);
         transform.localScale = new Vector3(scaleSize,scaleSize,scaleSize);
+
+        GetComponent<Rigidbody>().velocity = new Vector3(2,2,2);
     }
 
         // Update is called once per frame
     void Update()
     {
+        
         transform.RotateAround(target.transform.position, Vector3.forward,  rotateSpeed* Time.deltaTime);
     }
 }
